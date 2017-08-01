6197294f77518f9307961fd27bf146585da2d4cc
Godfrey Davis <gdavis42@cnm.edu>
2017-08-01 16:01:44 -0600

    getting the layout of the Persona

---
+++ documentation/index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Fader.com</title>
+	</head>
+	<body>
+		<h1>Fader.com</h1><br>
+		<h2>Persona:</h2>
+		<strong>Name:</strong><br>
+		<strong>Profession/Role:</strong><br>
+		<strong>Behavior/Attitude:</strong><br>
+		<strong>User-Story:</strong><br>
+
+
+	</body>
+</html>
